2bfbadcf95c4366ac59c5fdb5818122ac95a654a
Fasil hameed <130664334+FasilHameed@users.noreply.github.com>
2024-04-11 13:49:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,14 +33,14 @@ def read_sql_query(sql,db):
 prompt=[
     """
     You are an expert in converting English questions to SQL query!
-    The SQL database has the name STUDENT and has the following columns - NAME, CLASS, 
+    Read The Prompt Carefully and try to understand What The User Needs read the input from user and spend good time Understanding that,
+    The SQL database has the name STUDENT and has the following columns - NAME, CLASS,marks,section, 
     SECTION \n\nFor example,\nExample 1 - How many entries of records are present?, 
     the SQL command will be something like this SELECT COUNT(*) FROM STUDENT ;
     \nExample 2 - Tell me all the students studying in Data Science class?, 
     the SQL command will be something like this SELECT * FROM STUDENT 
     where CLASS="Data Science"; 
     also the sql code should not have ``` in beginning or end and sql word in output
-
     """
 ]
 
